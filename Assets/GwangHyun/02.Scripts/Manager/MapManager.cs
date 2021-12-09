6cbfbd76fafa4b52df9213821f48737454332e1e
kjpark102312@naver.com <kjpark102312@naver.com>
2021-12-10 00:45:26 +0900

    스테이지틀 완료

---
+++ Assets/GwangHyun/02.Scripts/Manager/MapManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    private static MapManager instance = null;
+
+    public List<GameObject> mapList = new List<GameObject>();
+
+    public static MapManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                Debug.Log("instance°¡ nullÀÔ´Ï´Ù.");
+
+                return null;
+            }
+
+            return instance;
+        }
+
+        private set
+        {
+            instance = value;
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.Log("Áßº¹µÈ instance ÀÔ´Ï´Ù.");
+            Destroy(this);
+            return;
+        }
+
+        Instance = this;
+    }
+
+}
