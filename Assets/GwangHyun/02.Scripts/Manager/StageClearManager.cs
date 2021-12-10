5fd1830e0a6379f6e16132abaf96446766fe3891
kjpark102312@naver.com <kjpark102312@naver.com>
2021-12-10 14:55:03 +0900

    스테이지 클리어 틀 제작

---
+++ Assets/GwangHyun/02.Scripts/Manager/StageClearManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageClearManager : MonoBehaviour
+{
+    private int nowStage;
+
+    private void Start()
+    {
+        nowStage = GameManager.Instance
+    }
+
+    public void StageClear()
+    {
+        PlayerPrefs.SetInt("stageUnlock", nowStage);
+
+        nowStage++;
+    }
+
+    public void NextStage()
+    {
+        GameManager.Instance.LoadScene(nowStage + 1);
+    }
+}
