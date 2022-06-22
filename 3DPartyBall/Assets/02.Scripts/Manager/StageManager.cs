63b048de3cd84106180fcf57ce1acf1a06b8725b
KMJ1324 <eppurdream@gmail.com>
2022-06-22 17:35:03 +0900

    기본적인 스테이지 코드 작성

--- 3DPartyBall/Assets/02.Scripts/Manager/StageManager.cs
+++ 3DPartyBall/Assets/02.Scripts/Manager/StageManager.cs
@@ -1,37 +1,66 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class StageManager : MonoBehaviour
 {
     public Transform content;   // 부모 Trm
 
-    private List<GameObject> stageObjs = new List<GameObject>(); // Resources에 저장되어 있는 스테이지들의 모임
+    private List<GameObject> _selectUIList = new List<GameObject>();    // Resources에 저장되어 있는 UI들의 모임
+    private Dictionary<string, GameObject> _stageDic = new Dictionary<string, GameObject>();       // Resources에 저장되어 있는 stage들의 모임 ( Instantiate 안한 상태 )
+
+    private GameObject _currentStage = null;
 
     private void Awake()
     {
-        GameObject[] objs = Resources.LoadAll<GameObject>("Maps");
+        GameObject[] uis = Resources.LoadAll<GameObject>("MapSelectUI"); // MapSelectUI 폴더 안에 있는 UI 오브젝트 모두 가져오기
+        GameObject[] stages = Resources.LoadAll<GameObject>("Stages");      // Stages 폴더 안에 있는 오브젝트 모두 가져옴
 
         // objs에 있는거 모두 stageObjs로 이동
-        foreach (GameObject obj in objs)
+        foreach (GameObject ui in uis)
         {
-            Debug.Log("이 맵이 추가됨 : " + obj.name);
-            GameObject o = Instantiate(obj, content);
+            GameObject o = Instantiate(ui, content);
+            StageSelectUI sv = o.GetComponent<StageSelectUI>();
 
-            StageValue sv = o.GetComponent<StageValue>();
             sv.startButton.onClick.AddListener(() =>
             {
                 LoadStage(sv.stageName);
             });
 
-            stageObjs.Add(o);
+            _selectUIList.Add(o);
+        }
+
+        foreach(GameObject stage in stages)
+        {
+            Debug.Log("들어간 스테이지 이름 : " + stage.name);
+            _stageDic.Add(stage.name, stage);
         }
     }
 
     public void LoadStage(string stageName)
-    {
-        Debug.Log(stageName + " 스테이지 불러오기!");
-        // 스테이지 불러오는 방식은 생각하기
+    { 
+        if (!(SceneManager.GetActiveScene().name == "Main"))
+            SceneManager.LoadScene("Main");
+        else
+        {
+            // 이미 생성되어 있는 스테이지 삭제
+            Destroy(_currentStage);
+        }
+
+        SceneManager.sceneLoaded += (scene, loadSceneMode) =>
+        {
+            Debug.Log("생성된 스테이지 이름 : " + stageName);
+            _currentStage = Instantiate(_stageDic[stageName]);
+        };
+
+        // 스테이지 생성하기
+
+        // 스테이지 불러오는 방식은 잘 생각하기
+        // 스테이지 이동 순서는 
+        // 1. 게임 씬 이동
+        // 2. 해당 스테이지 생성
+        // 이렇게 하는게 맞지 않을까
     }
 
 
--- 3DPartyBall/Assets/02.Scripts/UI/StageSelectUI.cs
+++ 3DPartyBall/Assets/02.Scripts/UI/StageSelectUI.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class StageValue : MonoBehaviour
+public class StageSelectUI : MonoBehaviour
 {
     [HideInInspector] public string stageName;      // 스테이지의 이름
     [HideInInspector] public Button startButton;    // 스테이지 시작하는 버튼
