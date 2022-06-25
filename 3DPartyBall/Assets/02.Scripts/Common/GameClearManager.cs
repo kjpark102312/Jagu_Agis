95237bbaf821fd7132e7ab052a1f5de6297af7a8
kjpark102312@naver.com <kjpark102312@naver.com>
2022-06-25 19:57:58 +0900

    UI 적용

---
+++ 3DPartyBall/Assets/02.Scripts/Common/GameClearManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class GameClearManager : MonoBehaviour
+{
+    public GameObject[] playerBalls;
+
+    [SerializeField] GameObject backgroundPanel;
+    [SerializeField] GameObject gameClearPanel;
+    public void GameClearCheck()
+    {
+        int ballCount = 0;
+        for(int i = 0; i < playerBalls.Length; i++)
+        {
+            if (!playerBalls[i].activeSelf)
+                ballCount++;
+        }
+
+        if (ballCount == playerBalls.Length)
+        {
+            backgroundPanel.SetActive(true);
+            gameClearPanel.transform.DOScale(1, 0.3f);
+        }
+
+    }
+}
--- 3DPartyBall/Assets/02.Scripts/Goal.cs
+++ 3DPartyBall/Assets/02.Scripts/Goal.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Goal : MonoBehaviour
 {
+    [SerializeField] GameObject gameClearPanel;
+
     public GameObject master;
+
+    GameClearManager clearManager;
 
     bool isGoal = false;
     float timer;
@@ -15,6 +20,8 @@ public class Goal : MonoBehaviour
 
     private void Start()
     {
+        clearManager = FindObjectOfType<GameClearManager>();
+
         posB = new Vector3(transform.position.x - 2f, transform.position.y - 0.2f, 14.5f);
         posC = new Vector3(transform.position.x + 2f, transform.position.y - 0.2f, 14.5f);
     }
@@ -22,6 +29,12 @@ public class Goal : MonoBehaviour
     IEnumerator DrawTrajectory()
     {
         yield return new WaitForSeconds(0.5f);
+
+        master.transform.DOScale(0, 1.5f).OnComplete(() =>
+        {
+            master.gameObject.SetActive(false);
+            clearManager.GameClearCheck();
+        });
 
         while (isGoal)
         {
@@ -54,11 +67,16 @@ public class Goal : MonoBehaviour
             master = other.gameObject;
             curPos = master.transform.position;
 
-            other.GetComponent<Rigidbody>().velocity = Vector3.zero;
-            other.GetComponent<Rigidbody>().useGravity = false;
+            Rigidbody rb = other.GetComponent<Rigidbody>();
+
+            rb.velocity = Vector3.zero;
+            rb.useGravity = false;
+
             other.GetComponent<PlayerMove>().isGoal = true;
 
             StartCoroutine(DrawTrajectory());
+
+            
         }
     }
 }
